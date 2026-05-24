f0863ef94afa7722e39b2723499e529131aec565
jash-28 <jashaggarwal28@gmail.com>
2026-05-24 16:43:28 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://dsa-playground-codelabs.onrender.com/api",
   withCredentials: true,
 });
 
-// attach token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
